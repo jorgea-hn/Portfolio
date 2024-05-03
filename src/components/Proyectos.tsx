be89fab30c418538ec924714feae6025ec644f22
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2024-05-02 20:02:30 -0500

    Correccion de proyectos

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -11,14 +11,14 @@ const Proyectos: React.FC = () => {
 
     const projectsWeb: Project[] = [
         {
-            title: "Adopta un arbol",
-            description: "Proyecto web que busca la concientizacion ambiental para una region de Argentina",
+            title: "Guardianes del Bosque",
+            description: "Desarrollo de un proyecto web para la adopción de árboles en Argentina, incentivando la reforestación. La plataforma permite adoptar árboles, seguir su crecimiento y aprender sobre la importancia de la conservación ambiental.",
             technology: ["React", "Django", "PostgresSQL"],
             repository:"https://github.com/No-Country/s12-20-m-python"
         },
         {
-            title: "Clon de Trello",
-            description: "Proyecto web que busca la concientizacion ambiental para una region de Argentina",
+            title: "Pagina de gestion de proyectos",
+            description: "Desarrollo de un proyecto web de gestión de proyectos inspirado en Trello, ofreciendo una plataforma intuitiva para organizar tareas, asignar responsabilidades y hacer un seguimiento del progreso.",
             technology: ["React", "Django", "PostgresSQL"],
             repository:"https://github.com/No-Country/c13-38-ft-python"
         },
@@ -29,13 +29,13 @@ const Proyectos: React.FC = () => {
     const projectsDatos: Project[] = [
         {
             title: "Reconocimiento de Emociones",
-            description: "Proyecto web que busca la concientizacion ambiental para una region de Argentina",
+            description: "Desarrollo de una App de escritorio que utiliza IA para reconocer emociones. La app genera un conjunto de datos que incluye la emoción predominante, su intensidad y otros indicadores relevantes, permitiendo la investigación de las emociones.",
             technology: ["Python", "OpenCV", "PowerBI", "Pandas", "Excel"],
             repository:"https://github.com/No-Country/s14-26-ft-data-bi"
         },
         {
             title: "Anlisis de Constructora",
-            description: "Proyecto web que busca la concientizacion ambiental para una region de Argentina",
+            description: "Desarrollo de una web app que permite a una empresa con múltiples constructoras analizar su desempeño mediante datos históricos y un modelo predictivo de IA. Esto les permite tomar mejores decisiones, reducir costos y aumentar la satisfacción del cliente",
             technology: ["Python", "SQL", "Streamlit", "Pandas"],
             repository:"https://github.com/No-Country/s13-25-n-data-bi"
         }
@@ -44,14 +44,14 @@ const Proyectos: React.FC = () => {
     return (
         <div id="proyectos" className="bg-gray-300 min-h-screen ">
             <h3 className="text-xl md:text-2xl lg:text-3xl text-center pt-16">Proyectos</h3>
-            <div className="flex flex-col md:flex-row justify-around mt-8">
-                <div className="flex flex-col items-center md:w-1/2 md:px-0 px-5">
+            <div className="flex flex-col md:flex-row justify-around mt-8 ">
+                <div className="flex flex-col items-center md:w-1/2 md:px-0 px-5 mb-12">
                     <h3 className="text-lg md:text-lg lg:text-xl text-center pb-5">Proyectos Web</h3>
                     {projectsWeb.map((project) => (
                         <Card key={project.title} project={project} />
                     ))}
                 </div>
-                <div className="flex flex-col items-center md:w-1/2 md:px-0 px-5">
+                <div className="flex flex-col items-center md:w-1/2 md:px-0 px-5 mb-12">
                     <h3 className="text-lg md:text-lg lg:text-xl text-center pb-5">Proyectos de Data</h3>
                     {projectsDatos.map((project) => (
                         <Card key={project.title} project={project} />
