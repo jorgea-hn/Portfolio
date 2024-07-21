a4260ac6a9ed71399c4670a722482f4d524d0308
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2024-07-21 11:54:42 -0500

    Agregate project and enlace

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,12 @@
 import github from "../assets/github-white.png";
+import enlace from "../assets/enlace.png";
 
 interface Project {
     title: string;
     description: string;
     technology: string[];
     repository: string;
+    url: string;
 }
 
 interface CardProps {
@@ -16,7 +18,10 @@ const Card: React.FC<CardProps> = ({ project }) => {
         <div className="bg-slate-950 w-full md:w-4/5 p-5 rounded-md my-2">
             <div className="flex justify-between">
                 <h3 className="text-white mb-2">{project.title}</h3>
-                <a href={project.repository}><img className="w-5 h-5" src={github} alt="icon-github"/></a>
+                <div className="flex">
+                    <a href={project.repository}><img className="w-5 h-5 mr-2" src={github} alt="icon-github"/></a>
+                    <a href={project.url}><img className="w-5 h-5" src={enlace} alt="icon-url"/></a>
+                </div>
             </div>
             <p className="text-slate-400 mb-4">{project.description}</p>
             <ul className="flex flex-wrap text-slate-300">
--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -5,6 +5,7 @@ interface Project {
     description: string;
     technology: string[];
     repository: string;
+    url:string;
 }
 
 const Proyectos: React.FC = () => {
@@ -14,13 +15,22 @@ const Proyectos: React.FC = () => {
             title: "Guardianes del Bosque",
             description: "Desarrollo de un proyecto web para la adopción de árboles en Argentina, incentivando la reforestación. La plataforma permite adoptar árboles, seguir su crecimiento y aprender sobre la importancia de la conservación ambiental.",
             technology: ["React", "Django", "PostgreSQL"],
-            repository: "https://github.com/No-Country/s12-20-m-python"
+            repository: "https://github.com/No-Country/s12-20-m-python",
+            url:"",
         },
         {
             title: "Página de gestión de proyectos",
             description: "Desarrollo de un proyecto web de gestión de proyectos inspirado en Trello, ofreciendo una plataforma intuitiva para organizar tareas, asignar responsabilidades y hacer un seguimiento del progreso.",
             technology: ["React", "Django", "PostgreSQL"],
-            repository: "https://github.com/No-Country/c13-38-ft-python"
+            repository: "https://github.com/No-Country/c13-38-ft-python",
+            url:"",
+        },
+        {
+            title: "Calculadora",
+            description: "Proyecto calculadora de operaciones basicas y que permite realizar el cambio de color de esta para mayor personalización.",
+            technology: ["HTML", "CSS", "JavaScript"],
+            repository: "https://github.com/No-Country/c13-38-ft-python",
+            url:"https://jorgea-hn.github.io/Calculadora_JS.github.io/",
         }
     ]
 
@@ -29,13 +39,15 @@ const Proyectos: React.FC = () => {
             title: "Reconocimiento de Emociones",
             description: "Desarrollo de una app de escritorio que utiliza IA para reconocer emociones. La app genera un conjunto de datos que incluye la emoción predominante, su intensidad y otros indicadores relevantes, permitiendo la investigación de las emociones.",
             technology: ["Python", "OpenCV", "PowerBI", "Pandas", "Excel"],
-            repository: "https://github.com/No-Country/s14-26-ft-data-bi"
+            repository: "https://github.com/No-Country/s14-26-ft-data-bi",
+            url:"",
         },
         {
             title: "Análisis de Constructora",
             description: "Desarrollo de una web app que permite a una empresa con múltiples constructoras analizar su desempeño mediante datos históricos y un modelo predictivo de IA. Esto les permite tomar mejores decisiones, reducir costos y aumentar la satisfacción del cliente.",
             technology: ["Python", "SQL", "Streamlit", "Pandas"],
-            repository: "https://github.com/No-Country/s13-25-n-data-bi"
+            repository: "https://github.com/No-Country/s13-25-n-data-bi",
+            url:"",
         }
     ]
 
